b76d94244721bf6a15ca77d732b3b1ffb976abf4
gloutch <ismael.herve@yahoo.com>
2018-03-09 17:28:16 +0100

    Oope I forget Camera Folder

---
+++ Unity Projects/MainProject/Assets/Camera/CameraFlow.cs
@@ -0,0 +1,40 @@
+﻿// Inspired by a Long Qian's code
+// Email: lqian8@jhu.edu
+
+using UnityEngine;
+using System.Collections;
+
+
+/*
+ * Replace the Unity camera view by the camera of the device
+ */
+public class CameraFlow: MonoBehaviour {
+    
+	private WebCamTexture webCameraTexture;
+	private Material camTextureHolder;
+
+
+	void Start() {
+
+		// Checks how many and which cameras are available on the device
+		for (int cameraIndex = 0; cameraIndex < WebCamTexture.devices.Length; cameraIndex++) {
+#if UNITY_EDITOR
+			// In editor mode, the camera is usually front facing
+			// grab any camera is fine for testing
+			webCameraTexture = new WebCamTexture(cameraIndex, Screen.width * 10, Screen.height * 10);
+#else
+			// grab the back facing camera on Android device
+			if (!WebCamTexture.devices[cameraIndex].isFrontFacing) {
+				webCameraTexture = new WebCamTexture(cameraIndex, Screen.width, Screen.height);                
+			}
+#endif
+		}
+		camTextureHolder = new Material(Shader.Find("Standard"));
+		camTextureHolder.mainTexture = webCameraTexture;
+		webCameraTexture.Play();
+	}
+
+	void OnRenderImage(RenderTexture src, RenderTexture dst) {
+		Graphics.Blit(camTextureHolder.mainTexture, dst);
+	}
+}
