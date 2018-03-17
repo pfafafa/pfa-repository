b85e400efb19ea735fd10ade47fd6708df7608b3
gloutch <ismael.herve@yahoo.com>
2018-03-17 18:34:31 +0100

    improve interface and try some stuff

--- unity/MainProject/Assets/Interface/LoadScene.cs
+++ unity/MainProject/Assets/Interface/LoadScene.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadScene : MonoBehaviour {
-	
+public class LoadScene: MonoBehaviour {
+
 	public void ChangeSceneTo (string sceneName) {
 		SceneManager.LoadScene (sceneName);
 	}
