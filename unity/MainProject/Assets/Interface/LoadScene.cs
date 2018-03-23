be3ed9fdf853fa9e405c692ef5e8f59f1b593532
timarr <arroyo.timothe@gmail.com>
2018-03-23 12:52:49 +0100

    suppression des prefab

--- unity/MainProject/Assets/Interface/LoadScene.cs
+++ unity/MainProject/Assets/Interface/LoadScene.cs
@@ -4,7 +4,6 @@ using UnityEngine.SceneManagement;
 public class LoadScene: MonoBehaviour {
 
 	public void ChangeSceneTo (string sceneName) {
-		Screen.orientation = ScreenOrientation.LandscapeLeft;
 		SceneManager.LoadScene (sceneName);
 	}
 }
